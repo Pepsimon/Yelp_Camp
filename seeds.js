3ecbd0cbe0136475b4f866123c1e83e588d8a43d
Peter Peturson Simonsen <objektivet@gmail.com>
2018-01-03 20:30:58 +0000

    Fix author in seed, refactor to include author object

--- seeds.js
+++ seeds.js
@@ -6,17 +6,26 @@ var data = [
     {
         name: "Clouds Rest",
         image: "https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?h=350&auto=compress&cs=tinysrgb",
-        description: "Lorem ipsum dolor amet ethical taxidermy succulents snackwave, truffaut beard blue bottle knausgaard before they sold out salvia. Kickstarter glossier kombucha, kale chips shaman woke bicycle rights marfa dreamcatcher portland lumbersexual sartorial. Deep v post-ironic etsy crucifix forage franzen. Four dollar toast iPhone chicharrones you probably haven't heard of them tilde crucifix. Snackwave tumblr raclette wayfarers poke paleo copper mug whatever ramps. Meggings viral four loko forage street art messenger bag butcher keffiyeh iceland chambray chartreuse typewriter swag whatever enamel pin. Blog trust fund salvia raclette farm-to-table four loko drinking vinegar."
+        description: "Lorem ipsum dolor amet ethical taxidermy succulents snackwave, truffaut beard blue bottle knausgaard before they sold out salvia. Kickstarter glossier kombucha, kale chips shaman woke bicycle rights marfa dreamcatcher portland lumbersexual sartorial. Deep v post-ironic etsy crucifix forage franzen. Four dollar toast iPhone chicharrones you probably haven't heard of them tilde crucifix. Snackwave tumblr raclette wayfarers poke paleo copper mug whatever ramps. Meggings viral four loko forage street art messenger bag butcher keffiyeh iceland chambray chartreuse typewriter swag whatever enamel pin. Blog trust fund salvia raclette farm-to-table four loko drinking vinegar.",
+        author: {
+            username: "Homer"
+        }
     },
     {
         name: "Forest Greens",
         image: "https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?h=350&auto=compress&cs=tinysrgb",
-        description: "Lorem ipsum dolor amet ethical taxidermy succulents snackwave, truffaut beard blue bottle knausgaard before they sold out salvia. Kickstarter glossier kombucha, kale chips shaman woke bicycle rights marfa dreamcatcher portland lumbersexual sartorial. Deep v post-ironic etsy crucifix forage franzen. Four dollar toast iPhone chicharrones you probably haven't heard of them tilde crucifix. Snackwave tumblr raclette wayfarers poke paleo copper mug whatever ramps. Meggings viral four loko forage street art messenger bag butcher keffiyeh iceland chambray chartreuse typewriter swag whatever enamel pin. Blog trust fund salvia raclette farm-to-table four loko drinking vinegar."
+        description: "Lorem ipsum dolor amet ethical taxidermy succulents snackwave, truffaut beard blue bottle knausgaard before they sold out salvia. Kickstarter glossier kombucha, kale chips shaman woke bicycle rights marfa dreamcatcher portland lumbersexual sartorial. Deep v post-ironic etsy crucifix forage franzen. Four dollar toast iPhone chicharrones you probably haven't heard of them tilde crucifix. Snackwave tumblr raclette wayfarers poke paleo copper mug whatever ramps. Meggings viral four loko forage street art messenger bag butcher keffiyeh iceland chambray chartreuse typewriter swag whatever enamel pin. Blog trust fund salvia raclette farm-to-table four loko drinking vinegar.",
+        author: {
+        username: "Homer"
+        }
     },
     {
         name: "Magic Surf",
         image: "https://images.pexels.com/photos/587976/pexels-photo-587976.jpeg?h=350&auto=compress&cs=tinysrgb",
-        description: "Lorem ipsum dolor amet ethical taxidermy succulents snackwave, truffaut beard blue bottle knausgaard before they sold out salvia. Kickstarter glossier kombucha, kale chips shaman woke bicycle rights marfa dreamcatcher portland lumbersexual sartorial. Deep v post-ironic etsy crucifix forage franzen. Four dollar toast iPhone chicharrones you probably haven't heard of them tilde crucifix. Snackwave tumblr raclette wayfarers poke paleo copper mug whatever ramps. Meggings viral four loko forage street art messenger bag butcher keffiyeh iceland chambray chartreuse typewriter swag whatever enamel pin. Blog trust fund salvia raclette farm-to-table four loko drinking vinegar."
+        description: "Lorem ipsum dolor amet ethical taxidermy succulents snackwave, truffaut beard blue bottle knausgaard before they sold out salvia. Kickstarter glossier kombucha, kale chips shaman woke bicycle rights marfa dreamcatcher portland lumbersexual sartorial. Deep v post-ironic etsy crucifix forage franzen. Four dollar toast iPhone chicharrones you probably haven't heard of them tilde crucifix. Snackwave tumblr raclette wayfarers poke paleo copper mug whatever ramps. Meggings viral four loko forage street art messenger bag butcher keffiyeh iceland chambray chartreuse typewriter swag whatever enamel pin. Blog trust fund salvia raclette farm-to-table four loko drinking vinegar.",
+        author: {
+            username: "Homer"
+        }
     }
 ];
 
@@ -38,7 +47,9 @@ function seedDB() {
                     Comment.create({
                         
                         text: "This place is great!",
-                        author: "Homer"
+                        author: {
+                            username: "Homer"
+                        }
                         
                     }, function(err, comment){
                         if(err) {
